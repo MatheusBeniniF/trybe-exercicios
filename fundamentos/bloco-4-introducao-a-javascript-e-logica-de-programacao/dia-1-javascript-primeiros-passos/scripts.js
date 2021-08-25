dea324280e4fdfba160c872dca83491a7e8bcc97
MatheusBeninif <matheus_benini@hotmail.com>
2021-08-25 16:02:14 -0300

    Exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,9 +1,12 @@
-let a = 54;
-let b = 90;
-if(a > b) {
-    console.log("a é maior");
+let a = 76;
+let b = 108;
+let c = 3;
+if (a > b && a > c){
+    console.log("a é maior dos três");
 }
-
+else if (b > a && b > c){
+    console.log("b é maior dos três");
+}
 else {
-    console.log("b é maior");
+    console.log("c é maior dos três");
 }
