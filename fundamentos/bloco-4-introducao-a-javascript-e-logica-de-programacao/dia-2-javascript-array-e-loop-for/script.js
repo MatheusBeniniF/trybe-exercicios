2afe21dfcb2b4dd68d20326991c483f33d2a9057
MatheusBeniniF <matheus_benini@hotmail.com>
2021-08-26 17:52:26 -0300

    Exercicio 1 e 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+console.log(numbers);
+let soma = 0;
+for(let s = 0; s < numbers.length; s++) {
+    soma = soma + numbers[s];
+    console.log(soma);
+}
+console.log(soma);
