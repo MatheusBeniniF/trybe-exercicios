89bfaf1f52367dd5c4426bc09aa15b6a7cb8ce62
MatheusBeniniF <matheus_benini@hotmail.com>
2021-08-27 18:28:04 -0300

    Ex 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,8 +1,18 @@
-let n = 5;
+let n = 3;
 let s = '*';
-let space =' ';
+let space = ' ';
+let p = n;
 
-for(let i = 0; i < n; i++) {
-    space = space + s;
-    console.log(space);
-}
+for(let i = 0; i < n; i += 1) {
+   for(let j = 0; j <= n; j += 1) {
+        if(j < p) {
+            space = space + ' ';
+         } 
+        else {
+            space = space + s;
+        }
+   }
+   console.log(space);
+   space = ' ';
+   p -= 1;
+};
