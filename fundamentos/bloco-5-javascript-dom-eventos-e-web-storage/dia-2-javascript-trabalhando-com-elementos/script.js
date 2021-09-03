b8e4f4b0dbff6ac75a659431d4403b0901221dd9
MatheusBeniniF <matheus_benini@hotmail.com>
2021-09-03 18:41:49 -0300

    Exercicios 1 e 2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,10 @@
+//Parte 1
+//1
+const addH1 = document.createElement('h1');
+addH1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
+document.body.appendChild(addH1);
+
+//2
+const mainClass = document.createElement('main');
+mainClass.className = 'main-content';
+document.body.appendChild(mainClass); 
