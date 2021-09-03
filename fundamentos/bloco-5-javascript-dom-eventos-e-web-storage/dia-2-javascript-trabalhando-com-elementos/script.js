1701b76d7716702a34b1f61d9fbf4a1b0c74d878
MatheusBeniniF <matheus_benini@hotmail.com>
2021-09-03 19:08:04 -0300

    Exercicios 7, 8 e 9

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -10,7 +10,7 @@ mainClass.className = 'main-content';
 document.body.appendChild(mainClass); 
 
 //3
-const = sectionClass = document.createElement('section');
+const sectionClass = document.createElement('section');
 sectionClass.className = 'center-content';
 mainClass.appendChild(sectionClass);
 
@@ -29,3 +29,24 @@ const rightSection = document.createElement('section');
 rightSection.className ='right-content';
 document.main.appendChild(rightSection);
 
+//7
+const image = document.createElement('img');
+image.className = 'small-image';
+image.src = 'https://picsum.photos/200';
+leftSection.appendChild(image);
+
+//8
+const unorderedList = document.createElement('ul');
+rightSection.appendChild(unorderedList);
+const array = ['Um','Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez'];
+for(let num in array) {
+  const orderedList = document.createElement('li');
+  orderedList.innerHTML = array[num];
+  unorderedList.appendChild(orderedList);
+}
+
+//9
+for (let i = 0; i < 3; i += 1) {
+const addH3 = document.createElement('h3');
+main.appendChild(addH3);
+}
