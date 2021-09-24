48548b63e61f937e3eb0f0eec3d0e944c2c6adb0
MatheusBeniniF <matheus_benini@hotmail.com>
2021-09-24 14:39:28 -0300

    Exercicio 1 parte 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -14,3 +14,12 @@ testingScope(true);
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`);
+
+const factorial = numberFac => {
+  let result = 1;
+  for (let i = 2; i <= numberFac; i += 1) {
+    result *= i;
+  }
+  return result;
+}
+console.log(factorial(3));
