4bc600a2668f61205093c0130c1de2ac777424f0
MatheusBeniniF <matheus_benini@hotmail.com>
2021-10-07 17:12:45 -0300

    Exercicio 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex6.js
@@ -1,5 +1,7 @@
+const assert = require('assert');
+
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
-const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const notes = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 const studentAvarage = () => {
   const nameAndAverage = students.map((student, index) => ({
@@ -8,3 +10,11 @@ const studentAvarage = () => {
   }));
   return nameAndAverage;
 }
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAvarage(), expected);
