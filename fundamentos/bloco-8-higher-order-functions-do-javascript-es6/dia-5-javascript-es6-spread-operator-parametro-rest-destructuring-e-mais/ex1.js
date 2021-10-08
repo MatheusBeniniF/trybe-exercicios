79eb343775e0c6d4fafd48ebb0f35bae8acc1248
MatheusBeniniF <matheus_benini@hotmail.com>
2021-10-08 15:34:35 -0300

    Exercicio 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/ex1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/ex1.js
@@ -14,3 +14,61 @@ rectangles.forEach((rectangle) => {
 // Exercicio 2
 // escreva sum abaixo
 const sum = (...numbers) => numbers.reduce(((acc, value) => acc + value), 0);
+
+// Exercicio 3
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({ name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log('Exercicio 3:')
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
+
+// Exercicio 4
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = (arr) => arr.filter (
+  ({ nationality, bornIn}) => 
+  nationality === 'Australian'
+  && bornIn > 1901 && bornIn <= 2000,
+)
