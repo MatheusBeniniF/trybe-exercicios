334fdeab190b1fbb565a80e715dda07e84f4c388
MatheusBeniniF <matheus_benini@hotmail.com>
2021-10-28 14:38:20 -0300

    Exercicio de fixaçao props

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Image from './Image';
+
+function App() {
+  return (
+    <main>
+      <Image source={logo} alternativeText="Cute cat staring" />
+    </main>
+  );
+}
+
+export default App;
