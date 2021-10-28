7115e399a4b7174e82970f810a8db1a886ca1984
MatheusBeniniF <matheus_benini@hotmail.com>
2021-10-27 21:48:13 -0300

    Exercicio 3 e 4

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
-const Task = (value) => {
-  return (
-    <li>{value}</li>
-  );
-}
-
-const tarefas = ['acordar', 'malhar', 'estudar', 'almocar'];
+import Header from './header';
+import Content from './Content';
+import Footer from './footer';
 
 class App extends Component {
   render () {
     return (
       <div>
-        <h1>Exercicio 1 do dia</h1>
-        <ul>{ tarefas.map(tarefa => Task(tarefa))}</ul>
+        <Header />
+        <Content />
+        <Footer />
       </div>
     );
   }
