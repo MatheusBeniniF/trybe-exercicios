33ebb809c6c46712638829cbd007711dd47e6a3e
MatheusBeniniF <matheus_benini@hotmail.com>
2022-03-25 11:27:47 -0300

    Ex do dia

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {console.log(`Porta ${PORT}`)})
